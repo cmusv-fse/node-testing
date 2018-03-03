33bbddd506f5aeaa8add1fad5acdbc076f71d4b6
erdogmush <erdogmush@gmail.com>
2018-03-02 16:02:56 -0800

    test for equality for sets

--- test/test-ex4.js
+++ test/test-ex4.js
@@ -1,14 +1,20 @@
 let expect = require('chai').expect;
 later = require('../lectureExamples/ex4').later;
 
-suite('Timeout test', function () {
+suite('Some Class Examples', function () {
 
-    test('can print whatever', function (done) {
+    test('async function later can produce result', function (done) {
         later("Whatever!", function (result) {
             // console.log(result);
             expect(result).to.equal("Whatever!");
             done();
         });
+    });
+
+    test('check set equality works', function (done) {
+        expect(new Set([1, 2, 3])).to.eql(new Set([2, 1, 3]));
+        expect([1, 2, 3]).to.not.eql([2, 1, 3]);
+        done();
     });
 
 });
