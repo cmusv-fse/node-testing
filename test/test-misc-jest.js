9bf84cf381d4df9fb43524da21cf5bd6d20461e4
erdogmush <erdogmush@gmail.com>
2019-10-06 16:44:02 -0700

    migrated test-misc to jest

--- test/test-misc-jest.js
+++ test/test-misc-jest.js
@@ -1,3 +1,6 @@
+jest.setTimeout(500);
+
+
 test('check set equality works', function (done) {
     expect(new Set([1, 2, 3])).toEqual(new Set([2, 1, 3]));
     expect([1, 2, 3]).not.toEqual([2, 1, 3]);
@@ -8,40 +11,34 @@ test('when done cb is omitted, test passes', function () {
     //
 });
 
-/*
-test('when done cb exists but you forget to call it, test fails', function (done) {
-    //
+test('when done cb exists but you forget to call it, test fails with timeout', function (done) {
+    // nothing here
 });
-*/
 
-test("michael's example, no err, should pass", function (done) {
-    // this.timeout(1000); // may not be needed
+test("callback encapsulating a promise, should pass", done => {
     foo("data", 0, function (err, res) {
-        expect(err).toBe.false;
+        expect(err).toBe(false);
         expect(res).toBe("right object");
         done();
     });
 });
 
-test("michael's example, with err, should pass", function (done) {
-    // this.timeout(1000); // may not be needed
+test("callback encapsulating a promise, err case, should pass", done => {
     foo("data", 1, function (err, res) {
-        expect(err).toBe.true;
+        expect(err).toBe(true);
+        expect(res).toBe("something is wrong");
         done();
     });
 });
 
-test("michael's example, with err, should fail with timeout", function (done) {
-    // this.timeout(1000); // may not be needed
+test("callback encapsulating a promise, should fail with timeout", done => {
     foo("data", 1, function (err, res) {
-        expect(err).toBe.false;
-        // expect(res).toBe("right object");
+        expect(err).toBe(false);
         done(); // done will never be called when assertion fails because of use of promise in called code
     });
 });
 
-test("michael's example, with err, should fail gracefully", function () {
-    // this.timeout(1000); // may not be needed
+test("promise encapsulating a promise, should fail gracefully", () => {
     return betterfoo("data", 1).then((res) => { // return the promise to inform mocha
         expect(res).toBe("right object");
     }).catch((err) => {
@@ -49,14 +46,22 @@ test("michael's example, with err, should fail gracefully", function () {
     });
 });
 
-test("michael's example, removed done, will always pass", function () {
-    // this.timeout(1000); // may not be needed
+test("async function encapsulating a promise, should pass", async () => {
+    let res = await evenbetterfoo("data", 0);
+    expect(res).toBe("right object");
+});
+
+test("variation: async function encapsulating a promise, should pass", async () => {
+    await expect(evenbetterfoo("data", 0)).resolves.toBe("right object");
+});
+
+test("callback encapsulating a promise, without done, will always pass", () => {
     foo("data", 0, function (err, res) {
-        expect(false).toBe.true;
+        expect(false).toBe(true);
     });
 });
 
-/* michael's function */
+/* callback encapsulating a promise  */
 function foo(mydata, myerr, cb) {
     let timeStamp = new Date()//set current date
 
@@ -79,16 +84,17 @@ function foo(mydata, myerr, cb) {
     };
 
     newInvestigation.save().then(function (res) {
+        //console.log(res);
         cb(false, res);
     }).catch((err) => {
         // console.log(err)
         cb(true, err);
     }).catch((err) => {  // this is necessary in case an error is thrown in the cb
-        // do nothing 
+        // console.log(err)
     });
 }
 
-/* michael's function */
+/* better version: promise encapsulating a promise */
 function betterfoo(mydata, myerr) {
     let timeStamp = new Date()//set current date
 
@@ -110,10 +116,35 @@ function betterfoo(mydata, myerr) {
         })
     };
 
-    return newInvestigation.save();
+    return newInvestigation.save(); // returns a promise
 }
 
-test("seemingly async, but effectively sync code", function () {
+/* even better version: async function encapsulating a promise */
+async function evenbetterfoo(mydata, myerr) {
+    let timeStamp = new Date()//set current date
+
+    let newInvestigation = {
+        timestamp: timeStamp,
+        data: mydata,
+        err: myerr
+    };
+    newInvestigation.save = function () {
+        return new Promise((resolve, reject) => {
+            // fake async function
+            setTimeout(function () {
+                if (newInvestigation.err) {
+                    reject("something is wrong");
+                } else {
+                    resolve("right object");
+                }
+            }, 500);
+        })
+    };
+
+    return await newInvestigation.save(); // returns a promise
+}
+
+test("seemingly async, but effectively sync code", () => {
     let add = function (x, y, callback) {
         callback(x + y);
     } // just returning result via callback, structurally async
@@ -125,7 +156,7 @@ test("seemingly async, but effectively sync code", function () {
     expect(tata).toBe(9);
 });
 
-test("singleton has single instance", function () {
+test("singleton has single instance", () => {
     let Singleton = require('../lectureExamples/singleton.js').Singleton;
 
     s = new Singleton();
